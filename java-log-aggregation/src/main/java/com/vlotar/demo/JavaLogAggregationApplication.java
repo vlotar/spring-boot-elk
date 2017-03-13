a539f0e8116212b0295b6fddf8baf9d6def37560
lotarvad <i!love!Alina!>
2017-03-13 14:49:19 +0100

    Added MDC to logstash appender configuration

--- java-log-aggregation/src/main/java/com/vlotar/demo/JavaLogAggregationApplication.java
+++ java-log-aggregation/src/main/java/com/vlotar/demo/JavaLogAggregationApplication.java
@@ -1,7 +1,10 @@
 package com.vlotar.demo;
 
+import ch.qos.logback.classic.helpers.MDCInsertingServletFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Spring Boot application launcher
@@ -9,8 +12,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaLogAggregationApplication {
 
-	public static void main(String[] args) {
-		//starts spring application (embedded tomcat, security, logging etc.)
-		SpringApplication.run(JavaLogAggregationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        //starts spring application (embedded tomcat, security, logging etc.)
+        SpringApplication.run(JavaLogAggregationApplication.class, args);
+    }
+
+    /**
+     * A servlet filter that inserts various values retrieved from the incoming http
+     * request into the MDC
+     *
+     * @return {@link FilterRegistrationBean}
+     */
+    @Bean
+    public FilterRegistrationBean userInsertingMdcFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        MDCInsertingServletFilter userFilter = new MDCInsertingServletFilter();
+        registrationBean.setFilter(userFilter);
+        return registrationBean;
+    }
 }
