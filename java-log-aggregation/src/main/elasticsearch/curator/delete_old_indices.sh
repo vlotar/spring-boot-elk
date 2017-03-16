2a2420239add776fbaf4e87c69bbb078b9edeee5
lotarvad <i!love!Alina!>
2017-03-16 09:38:07 +0100

    RabbitMQ as input for logstash. First draft.

---
+++ java-log-aggregation/src/main/elasticsearch/curator/delete_old_indices.sh
@@ -0,0 +1,1 @@
+docker run -v /Users/lotarvad/Development/git/vlotar/spring-boot-elk/java-log-aggregation/src/main/elasticsearch/curator:/root/.curator/ --rm bobrik/curator /root/.curator/delete_indices.yml
